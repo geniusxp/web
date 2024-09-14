131ed868ca2be83ad75b87b21bd2f9200c9b428a
ericknathan <erick.capito@hotmail.com>
2024-09-14 15:46:56 -0300

    feat: create event map page

---
+++ src/app/events/[id]/event-map/event-map-upload.tsx
@@ -0,0 +1,75 @@
+"use client";
+
+import { Button, buttonVariants } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { UploadIcon } from "lucide-react";
+import { useState } from "react";
+import { toast } from "sonner";
+
+export default function EventMapUpload() {
+  const [image, setImage] = useState<File | null>(null);
+  const [imagePreview, setImagePreview] = useState<string | null>(
+    "https://i.ibb.co/9V2sFf5/image-map.png"
+  );
+
+  const handleImageChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const file = e.target.files?.[0];
+    if (file) {
+      if (file.type.startsWith("image/")) {
+        setImage(file);
+        const reader = new FileReader();
+        reader.onloadend = () => {
+          setImagePreview(reader.result as string);
+        };
+        reader.readAsDataURL(file);
+      } else {
+        toast.error("Por favor, selecione um arquivo de imagem.");
+        setImage(null);
+        setImagePreview(null);
+      }
+    }
+  };
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    if (image) {
+      console.log("Uploading image:", image.name);
+    } else {
+      toast.error("Por favor, selecione um arquivo de imagem.");
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit} className="space-y-4">
+      <div className="aspect-video w-full bg-card rounded-lg overflow-auto">
+        {imagePreview ? (
+          <img src={imagePreview} alt="" className="min-w-full min-h-full" />
+        ) : (
+          <div className="size-full flex items-center justify-center">
+            <p className="text-muted-foreground">Nenhum mapa selecionado</p>
+          </div>
+        )}
+      </div>
+      <footer className="flex items-center justify-between">
+        <Input
+          id="imageUpload"
+          type="file"
+          accept="image/*"
+          onChange={handleImageChange}
+          className="hidden"
+        />
+        <Label
+          htmlFor="imageUpload"
+          className={buttonVariants({ variant: "outline", className: "cursor-pointer" })}
+        >
+          <UploadIcon className="mr-2 h-4 w-4" />
+          Adicionar mapa
+        </Label>
+        <Button type="submit" disabled={!image}>
+          Salvar alterações
+        </Button>
+      </footer>
+    </form>
+  );
+}
--- src/app/events/[id]/event-map/page.tsx
+++ src/app/events/[id]/event-map/page.tsx
@@ -1,11 +1,16 @@
 import { Header } from "@/components/app/header";
+import EventMapUpload from "./event-map-upload";
 
 export default function EventMapPage() {
   return (
     <main className="flex-1">
       <Header eventName="FIAP Next 2024" pageName="Mapa do evento" />
 
-      <div className="p-4 grid gap-4"></div>
+      <div className="p-4 grid gap-4 animate-in slide-in-from-bottom-8 fade-in duration-1000">
+        <div className="w-3/4 mx-auto">
+          <EventMapUpload />
+        </div>
+      </div>
     </main>
   );
 }
