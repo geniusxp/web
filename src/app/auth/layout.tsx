0c40ea66c0620c82c32694a64cc992e8ad3a4526
ericknathan <erick.capito@hotmail.com>
2024-10-09 22:43:19 -0300

    fix: auth pages responsiviness

--- src/app/auth/layout.tsx
+++ src/app/auth/layout.tsx
@@ -6,11 +6,10 @@ export default function AuthLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <div className="grid grid-cols-2 w-full">
-      <div className="sticky top-0 h-screen flex flex-col justify-between items-start bg-[url(/auth-background.png)] bg-no-repeat bg-cover bg-zinc-900 p-12">
+    <div className="grid md:grid-cols-2 w-full">
+      <div className="md:sticky top-0 md:h-screen flex flex-col justify-between items-start bg-[url(/auth-background.png)] bg-no-repeat bg-cover bg-zinc-900 p-12 max-md:gap-8">
         <div className="flex items-center gap-2">
           <Image src="/logo.svg" alt="GeniusXP" width={100} height={120} />
-          <span className="text-xl font-bold">Manager</span>
         </div>
         <p className="text-lg max-w-[463px]">
           A melhor experiência em eventos que você já viu! Faça login para obter
