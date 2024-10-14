9d2742b7d8892228f645e71b6b30d33e304dacf2
ericknathan <erick.capito@hotmail.com>
2024-10-14 20:01:12 -0300

    refactor: use links from variable on footer

--- src/components/app/footer.tsx
+++ src/components/app/footer.tsx
@@ -1,34 +1,55 @@
 import Image from "next/image";
 
+const footerSections = [
+  {
+    title: "Sobre",
+    links: [
+      { label: "Por que o GeniusXP?", href: "#" },
+      { label: "Equipe", href: "#" },
+      { label: "Blog", href: "#" },
+      { label: "Imprensa", href: "#" },
+    ],
+  },
+  {
+    title: "Suporte",
+    links: [
+      { label: "Central de ajuda", href: "#" },
+      { label: "Fale conosco", href: "#" },
+      { label: "Reportar abuso", href: "#" },
+    ],
+  },
+  {
+    title: "Legal",
+    links: [
+      { label: "Diretrizes", href: "#" },
+      { label: "Termos de Serviço", href: "#" },
+      { label: "Política de Privacidade", href: "#" },
+    ],
+  }
+]
+
 export function Footer() {
   return (
     <footer className="container">
       <div className="flex w-full flex-wrap justify-between gap-8 px-6 py-12 md:px-0 border-y">
-        <Image src="/logo.svg" alt="GeniusXP" width={100} height={120} className="mb-auto" />
+        <Image
+          src="/logo.svg"
+          alt="GeniusXP"
+          width={100}
+          height={120}
+          className="mb-auto"
+        />
         <div className="grid grid-cols-3 gap-8">
-          <div className="grid text-xs leading-relaxed text-muted-foreground">
-            <p className="mb-1.5 text-base font-semibold text-foreground">Sobre</p>
-            <a href="">Por que o GeniusXP?</a>
-            <a href="">Equipe</a>
-            <a href="">Blog</a>
-            <a href="">Imprensa</a>
-          </div>
-
-          <div className="grid text-xs leading-relaxed text-muted-foreground">
-            <p className="mb-1.5 text-base font-semibold text-foreground">
-              Suporte
-            </p>
-            <a href="">Central de ajuda</a>
-            <a href="">Fale conosco</a>
-            <a href="">Reportar abuso</a>
-          </div>
-
-          <div className="grid text-xs leading-relaxed text-muted-foreground">
-            <p className="mb-1.5 text-base font-semibold text-foreground">Legal</p>
-            <a href="">Diretrizes</a>
-            <a href="">Termos de Serviço</a>
-            <a href="">Política de Privacidade</a>
-          </div>
+          {footerSections.map(({ title, links }) => (
+            <div key={title} className="grid text-xs leading-relaxed text-muted-foreground">
+              <p className="mb-1.5 text-base font-semibold text-foreground">
+                {title}
+              </p>
+              {links.map(({ label, href }) => (
+                <a key={label} href={href} className="hover:underline underline-offset-2 hover:text-foreground">{label}</a>
+              ))}
+            </div>
+          ))}
         </div>
       </div>
       <div className="flex w-full flex-col justify-between gap-6 px-6 py-8 md:px-0">
