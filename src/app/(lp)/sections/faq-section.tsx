aac714adf00f729aeb8cf959bfa3d54bc9067b82
ericknathan <erick.capito@hotmail.com>
2024-10-14 20:00:59 -0300

    feat: add faq questions correct texts

--- src/app/(lp)/sections/faq-section.tsx
+++ src/app/(lp)/sections/faq-section.tsx
@@ -7,24 +7,41 @@ import {
 
 const questions = [
   {
-    question: "Pergunta 1?",
+    question: "O que é o GeniusXP?",
     answer:
-      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quidem sunt, quas recusandae exercitationem tempora dignissimos, veritatis laboriosam quos sit, tempore veniam iste quaerat sed nemo! Ipsam molestiae quo aut excepturi?",
+      "O GeniusXP é uma plataforma de gerenciamento 360 de eventos, que utiliza inteligência artificial para otimizar a experiência dos participantes e organizadores. Oferecemos desde a preparação e compra até a análise pós-evento, gerando insights valiosos e ajudando na conversão de leads.",
   },
   {
-    question: "Pergunta 2?",
+    question: "Como utilizamos Inteligência Artificial?",
     answer:
-      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quidem sunt, quas recusandae exercitationem tempora dignissimos, veritatis laboriosam quos sit, tempore veniam iste quaerat sed nemo! Ipsam molestiae quo aut excepturi?",
+      "A inteligência artificial é utilizada para analisar sentimentos dos participantes, gerar recomendações personalizadas de estandes e projetos, prever tendências de interação, e automatizar tarefas como o envio de e-mails personalizados e a análise de emoções.",
   },
   {
-    question: "Pergunta 3?",
+    question: "Quais tipos de eventos o GeniusXP pode gerenciar?",
     answer:
-      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quidem sunt, quas recusandae exercitationem tempora dignissimos, veritatis laboriosam quos sit, tempore veniam iste quaerat sed nemo! Ipsam molestiae quo aut excepturi?",
+      "O GeniusXP é flexível e pode ser utilizado em diversos tipos de eventos, como conferências, feiras, workshops, hackathons, seminários e outros eventos corporativos ou acadêmicos.",
   },
   {
-    question: "Pergunta 4?",
+    question: "Que tipos de dados são coletados e analisados pelo GeniusXP?",
     answer:
-      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quidem sunt, quas recusandae exercitationem tempora dignissimos, veritatis laboriosam quos sit, tempore veniam iste quaerat sed nemo! Ipsam molestiae quo aut excepturi?",
+      "Coletamos dados como interações dos participantes, locais mais frequentados, vendas, feedback em enquetes, análise de emoções, entre outros. Esses dados ajudam a gerar insights detalhados sobre o comportamento e preferências dos participantes.",
+  },
+  {
+    question: "Quais são os benefícios para os organizadores de eventos?",
+    answer:
+      "Os organizadores podem otimizar a logística do evento, entender melhor o comportamento dos participantes, identificar os momentos de maior interação, e gerar relatórios automáticos com insights detalhados para aprimorar eventos futuros.",
+  },
+  {
+    question:
+      "Como o GeniusXP ajuda a aumentar o engajamento dos participantes?",
+    answer:
+      "Utilizamos gamificação, recomendações personalizadas e análise de sentimentos para oferecer experiências únicas e interativas. Isso ajuda a manter os participantes engajados durante todo o evento.",
+  },
+  {
+    question:
+      "Como posso integrar o GeniusXP com outras ferramentas que já utilizo?",
+    answer:
+      "Resposta: O GeniusXP possui APIs e opções de integração que permitem conectá-lo a diversas ferramentas de CRM, marketing e análise de dados. Entre em contato com o nosso suporte para mais informações sobre integrações específicas.",
   },
 ];
 
