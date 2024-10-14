4324753bb2bdc163c79c32cad98a5e1d72780c3b
ericknathan <erick.capito@hotmail.com>
2024-10-14 12:20:42 -0300

    feat: add vercel analytics

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -4,12 +4,14 @@ import { Toaster } from "@/components/ui/sonner";
 import { queryClient } from "@/lib/react-query";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { ReactNode } from "react";
+import { Analytics } from "@vercel/analytics/react";
 
 export function Providers({ children }: { children: ReactNode }) {
   return (
     <QueryClientProvider client={queryClient}>
       {children}
       <Toaster />
+      <Analytics />
     </QueryClientProvider>
   );
 }
